e9fcf920bd792bd834b962b4f3515b1693d9f6dd
thitichat-11 <thitichatphoto@gmail.com>
2026-07-16 00:51:33 +0700

    วางโครงหน้า ComeOnTrend

--- frontend/src/pages/user/ComeOnTrend.jsx
+++ frontend/src/pages/user/ComeOnTrend.jsx
@@ -2,7 +2,24 @@ import React from 'react'
 
 const ComeOnTrend = () => {
   return (
-    <div>ComeOnTrend</div>
+    <>
+        <div className='display-4 container text-center use-abril mt-4'>
+            <span>COME</span> &nbsp;
+            <span className=' text-danger'>ON</span> &nbsp;
+            <span>TREND</span> &nbsp;
+        </div>
+
+        {/* รูปภาพ */}
+        <div>
+            <img src="" alt="" />
+        </div>
+
+        <div className='container text-center use-akatab mt-5'>
+            <p className='fw-bold'>THE ITEM</p>
+        </div>
+
+        <p className='text-center'>A signature piece for a uniquely curated wardrobe - one trend-setting item every week.</p>
+    </>
   )
 }
 
