88423c180e9b2361d3594e953d81a689c864ccc4
3ossU <surawat.san@spmail.net>
2026-07-16 21:15:39 +0700

    api Get http://localhost:5000/api/products แสดงข้อมูลในตาราง product

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config'; 
 import categoryRoutes from './src/Routes/categoryRoutes.js';
+import productRoutes from './src/Routes/productRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -10,6 +11,9 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/categories', categoryRoutes);
+app.use('/api/products', productRoutes);
+
+
 
 app.get('/', (req, res) => {
     res.send('SALA E-Commerce Backend API is running smoothly! 🚀');
